a253acac5f332b62d7ccbf1743c82b661eccb3fd
mryan1312 <129277943+mryan1312@users.noreply.github.com>
2023-05-02 19:59:55 -0500

    comments

--- Mooder/Mooder.py
+++ Mooder/Mooder.py
@@ -1,14 +1,14 @@
-import csv
-import os
-import datetime
+import csv                                                                          # This app will let a user record their mood rating 1-5 with notes, date, and time
+import os                                                                           # Intention is to be able to plot the records on charts to track mood ratings over time
+import datetime                                                                     # There is an option to clear data, perhaps future could add encryption and password protection.
 
 def mood_record(rating):
-    if os.path.isfile("mood_history.csv") == False:
+    if os.path.isfile("mood_history.csv") == False:                                 # Check if file exists
         with open("mood_history.csv", 'w', newline='') as file:
             record = csv.writer(file)
-            record.writerow(["Date", "Time", "Rating", "Comment"])
+            record.writerow(["Date", "Time", "Rating", "Comment"])                  # Creating the file with headers
         file.close()
-    history = open("mood_history.csv", 'a', newline = '')
+    history = open("mood_history.csv", 'a', newline = '')                           # Opening the file and writing the new rating
     writehistory = csv.writer(history)
     writehistory.writerow(rating)
     history.close()
@@ -17,7 +17,7 @@ def main():
     rating = []
     os.system('cls')
     print("Hey! How are you doing?")
-    rating.append(str(datetime.datetime.now())[:10])
+    rating.append(str(datetime.datetime.now())[:10])                                # Just the date, then just the time
     rating.append(str(datetime.datetime.now())[11:19])
     rate = float(input("Between 1-5, how do you rate your mood?\n"))
     if rate >= 1 and rate <= 5:
